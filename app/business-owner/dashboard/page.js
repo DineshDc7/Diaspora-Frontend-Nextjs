5ea6930009aee1134f6348ea9f59b958e6f72564
asmitaliangtuang <asmita.liangtuang@gmail.com>
2026-01-05 19:15:41 +0530

    commit

---
+++ app/business-owner/dashboard/page.js
@@ -0,0 +1,17 @@
+"use client";
+
+import BusinessLayout from "../components/BusinessLayout";
+
+export default function DashboardPage() {
+  return (
+    <BusinessLayout title="Dashboard" subtitle="Live portfolio overview">
+      <div className="grid grid-cols-1 xl:grid-cols-4 gap-6 mt-6">
+        
+        <div className="xl:col-span-4 space-y-6">
+            <h1>fsdfgsudfhjk</h1>
+        </div>
+
+      </div>
+    </BusinessLayout>
+  );
+}
