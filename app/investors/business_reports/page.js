9a8c5a9599cb27ac94da61370cc2352d6b3d97e2
asmitaliangtuang <asmita.liangtuang@gmail.com>
2026-01-14 17:44:48 +0530

    commit

--- app/investors/business_reports/page.js
+++ app/investors/business_reports/page.js
@@ -70,7 +70,6 @@ export default function BusinessReport() {
   const [currentPage, setCurrentPage] = useState(1);
 
   const totalPages = Math.ceil(reports.length / ITEMS_PER_PAGE);
-
   const paginatedReports = reports.slice(
     (currentPage - 1) * ITEMS_PER_PAGE,
     currentPage * ITEMS_PER_PAGE
@@ -88,159 +87,153 @@ export default function BusinessReport() {
           <div className="xl:col-span-4 space-y-6">
             <div className="shadow-lg rounded-lg bg-white p-4">
               <div className="mb-5 md:mb-0">
-              <h5 className="text-sm font-semibold mb-2 text-gray-700">
-                Filter by Business:
-              </h5>
+                <h5 className="text-sm font-semibold mb-2 text-gray-700">
+                  Filter by Business:
+                </h5>
 
-              <div className="flex items-center justify-between gap-4">
-                {/* Select */}
-                <div className="w-[60dvw] md:w-[250px]">
-                  <Select
-                    value={selectedBusiness ?? ""}
-                    onValueChange={(value) => setSelectedBusiness(value)}
-                  >
-                    <SelectTrigger className="w-full">
-                      <SelectValue placeholder="Select business" />
-                    </SelectTrigger>
-                    <SelectContent>
-                      {businesses.map((business) => (
-                        <SelectItem key={business} value={business}>
-                          {business}
-                        </SelectItem>
-                      ))}
-                    </SelectContent>
-                  </Select>
-                </div>
+                <div className="flex items-center justify-between gap-4">
+                  {/* Select */}
+                  <div className="w-[60dvw] md:w-[250px]">
+                    <Select
+                      value={selectedBusiness ?? ""}
+                      onValueChange={(value) => setSelectedBusiness(value)}
+                    >
+                      <SelectTrigger className="w-full">
+                        <SelectValue placeholder="Select business" />
+                      </SelectTrigger>
+                      <SelectContent>
+                        {businesses.map((business) => (
+                          <SelectItem key={business} value={business}>
+                            {business}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                  </div>
 
-                {/* Clear Filter */}
-                {/* {selectedBusiness && ( */}
-                <button
-                  onClick={() => setSelectedBusiness(null)}
-                  className="text-sm font-semibold text-blue-600 hover:underline"
-                >
-                  Clear Filter
-                </button>
-                {/* )} */}
-              </div>
-            </div>
-
-            {/* <div className="bg-neutral-50 p-4 mt-3"> */}
-            <div className="py-5 overflow-x-auto">
-              <table className="border-collapse  min-w-[800px] w-full table-fixed p-2 border border-[#f1f3f7]">
-                <thead>
-                  <tr className="secondaryColor text-left subHeadingColor text-base">
-                    <th className="text-start p-4 border-b border-gray-100 w-[18%]">
-                      <h5 className="subHeadingColor text-base">Business</h5>
-                    </th>
-                    <th className="text-center p-4 border-b border-gray-100 w-[13%]">
-                      <h5 className="subHeadingColor text-base">Date</h5>
-                    </th>
-                    <th className="text-center p-4 border-b border-gray-100 w-[16%]">
-                      <h5 className="subHeadingColor text-base">Sales</h5>
-                    </th>
-                    <th className="text-center p-4 border-b border-gray-100 w-[13%]">
-                      <h5 className="subHeadingColor text-base">Expenses</h5>
-                    </th>
-                    <th className="text-center p-4 border-b border-gray-100 w-[12%]">
-                      <h5 className="subHeadingColor text-base">Customers</h5>
-                    </th>
-                    <th className="text-center p-4 border-b border-gray-100 w-[15%]">
-                      <h5 className="subHeadingColor text-base">Profit/Loss</h5>
-                    </th>
-                    <th className="text-center p-4 border-b border-gray-100 w-[13%]">
-                      <h5 className="subHeadingColor text-base">Actions</h5>
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {paginatedReports.map((row, index) => (
-                    <tr key={index} className="hover:bg-gray-50">
-                      <td className="p-4">
-                        <h4 className="headingColor font-semibold text-sm">
-                          {row.business}
-                        </h4>
-                      </td>
-
-                      <td className="p-4 text-center">
-                        <p className="textColor text-sm font-semibold">
-                          {row.date}
-                        </p>
-                      </td>
-
-                      <td className="p-4 text-center">
-                        <p className="text-green-600 text-sm font-semibold">
-                          {row.sales}
-                        </p>
-                      </td>
-
-                      <td className="p-4 text-center">
-                        <p className="text-red-600 text-sm font-semibold">
-                          {row.expenses}
-                        </p>
-                      </td>
-
-                      <td className="p-4 text-center">
-                        <p className="textColor text-sm font-semibold">
-                          {row.customers}
-                        </p>
-                      </td>
-
-                      <td className="p-4 text-center">
-                        <p className="text-green-700 text-sm font-semibold">
-                          {row.profit}
-                        </p>
-                      </td>
-
-                      <td className="p-4">
-                        <button
-                          onClick={() => setOpenModel(true)}
-                          className="flex gap-2 mx-auto items-center textprimaryColor text-sm font-semibold"
-                        >
-                          <Eye className="w-5 h-5" /> View
-                        </button>
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
-            <div className="flex justify-between items-center mt-4">
-              <p className="text-sm text-gray-500">
-                Page {currentPage} of {totalPages}
-              </p>
-
-              <div className="flex gap-2">
-                <button
-                  disabled={currentPage === 1}
-                  onClick={() => setCurrentPage((p) => p - 1)}
-                  className="px-3 py-1 text-sm border rounded-md disabled:opacity-50"
-                >
-                  Prev
-                </button>
-
-                {[...Array(totalPages)].map((_, i) => (
+                  {/* Clear Filter */}
+                  {/* {selectedBusiness && ( */}
                   <button
-                    key={i}
-                    onClick={() => setCurrentPage(i + 1)}
-                    className={`px-3 py-1 text-sm border rounded-md ${
-                      currentPage === i + 1
-                        ? "bg-gray-900 text-white"
-                        : "bg-white"
-                    }`}
+                    onClick={() => setSelectedBusiness(null)}
+                    className="text-sm font-semibold text-blue-600 hover:underline"
                   >
-                    {i + 1}
+                    Clear Filter
                   </button>
-                ))}
-
-                <button
-                  disabled={currentPage === totalPages}
-                  onClick={() => setCurrentPage((p) => p + 1)}
-                  className="px-3 py-1 text-sm border rounded-md disabled:opacity-50"
-                >
-                  Next
-                </button>
+                  {/* )} */}
+                </div>
               </div>
-            </div>
+
+              {/* <div className="bg-neutral-50 p-4 mt-3"> */}
+              <div className="py-5 overflow-x-auto">
+                <table className="border-collapse  min-w-[800px] w-full table-fixed p-2 border border-[#f1f3f7]">
+                  <thead>
+                    <tr className="secondaryColor text-left subHeadingColor text-base">
+                      <th className="text-start p-4 border-b border-gray-100 w-[18%]">
+                        <h5 className="subHeadingColor text-base">Business</h5>
+                      </th>
+                      <th className="text-center p-4 border-b border-gray-100 w-[13%]">
+                        <h5 className="subHeadingColor text-base">Date</h5>
+                      </th>
+                      <th className="text-center p-4 border-b border-gray-100 w-[16%]">
+                        <h5 className="subHeadingColor text-base">Sales</h5>
+                      </th>
+                      <th className="text-center p-4 border-b border-gray-100 w-[13%]">
+                        <h5 className="subHeadingColor text-base">Expenses</h5>
+                      </th>
+                      <th className="text-center p-4 border-b border-gray-100 w-[12%]">
+                        <h5 className="subHeadingColor text-base">Customers</h5>
+                      </th>
+                      <th className="text-center p-4 border-b border-gray-100 w-[15%]">
+                        <h5 className="subHeadingColor text-base">
+                          Profit/Loss
+                        </h5>
+                      </th>
+                      <th className="text-center p-4 border-b border-gray-100 w-[13%]">
+                        <h5 className="subHeadingColor text-base">Actions</h5>
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {paginatedReports.map((row, index) => (
+                      <tr key={index} className="hover:bg-gray-50">
+                        <td className="p-4">
+                          <h4 className="headingColor font-semibold text-sm">
+                            {row.business}
+                          </h4>
+                        </td>
+                        <td className="p-4 text-center">
+                          <p className="textColor text-sm font-semibold">
+                            {row.date}
+                          </p>
+                        </td>
+                        <td className="p-4 text-center">
+                          <p className="text-green-600 text-sm font-semibold">
+                            {row.sales}
+                          </p>
+                        </td>
+                        <td className="p-4 text-center">
+                          <p className="text-red-600 text-sm font-semibold">
+                            {row.expenses}
+                          </p>
+                        </td>
+                        <td className="p-4 text-center">
+                          <p className="textColor text-sm font-semibold">
+                            {row.customers}
+                          </p>
+                        </td>
+                        <td className="p-4 text-center">
+                          <p className="text-green-700 text-sm font-semibold">
+                            {row.profit}
+                          </p>
+                        </td>
+                        <td className="p-4">
+                          <button
+                            onClick={() => setOpenModel(true)}
+                            className="flex gap-2 mx-auto items-center textprimaryColor text-sm font-semibold"
+                          >
+                            <Eye className="w-5 h-5" /> View
+                          </button>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+              <div className="flex justify-between items-center mt-4">
+                <p className="text-sm text-gray-500">
+                  Page {currentPage} of {totalPages}
+                </p>
+
+                <div className="flex gap-2">
+                  <button
+                    disabled={currentPage === 1}
+                    onClick={() => setCurrentPage((p) => p - 1)}
+                    className="px-3 py-1 text-sm border rounded-md disabled:opacity-50">
+                    Prev
+                  </button>
+
+                  {[...Array(totalPages)].map((_, i) => (
+                    <button
+                      key={i}
+                      onClick={() => setCurrentPage(i + 1)}
+                      className={`px-3 py-1 text-sm border rounded-md ${
+                        currentPage === i + 1
+                          ? "bg-gray-900 text-white"
+                          : "bg-white"
+                      }`}
+                    >
+                      {i + 1}
+                    </button>
+                  ))}
+
+                  <button
+                    disabled={currentPage === totalPages}
+                    onClick={() => setCurrentPage((p) => p + 1)}
+                    className="px-3 py-1 text-sm border rounded-md disabled:opacity-50">
+                    Next
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
